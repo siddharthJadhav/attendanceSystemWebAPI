1ce0b688999018e9d7204a288ffdc9605b478b0a
siddharthjadhav <jadhavsiddharth9320@gmail.com>
2019-12-04 19:29:53 +0530

    add validation

--- Models/EmployeesModel.cs
+++ Models/EmployeesModel.cs
@@ -1,17 +1,37 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace AttendanceSystemWebAPI.Models
 {
 	public class EmployeesModel
 	{
 		public int ID { get; set; }
 
-		public string FirstName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
 
+        //public string MiddleName { get; set; }
+
+        [Required]
 		public string LastName { get; set; }
 
+        [Required]
 		public string Gender { get; set; }
 
-		public int Salary { get; set; }
+        public int Salary { get; set; }
 
-	}
+		//public string Designation { get; set; }
+
+        //public string Shift { get; set; }
+
+        //public string Address { get; set; }
+
+        //public string Email { get; set; }
+
+        //[Column(TypeName = "Date")]
+        //public DateTime DOB { get; set; }
+
+        //public DateTime JoiningDate { get; set; }
+    }
 }
